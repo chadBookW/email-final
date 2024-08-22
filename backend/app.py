a26a64175643b90892eec101e93246fca4c4a512
Nipun <nipunmanavya13@gmail.com>
2024-08-22 18:09:41 +0530

    Fix path to credentials.json

--- backend/app.py
+++ backend/app.py
@@ -50,7 +50,7 @@ def load_credentials():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('backend/credentials.json', SCOPES)
             creds = flow.run_local_server(port=8080)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
